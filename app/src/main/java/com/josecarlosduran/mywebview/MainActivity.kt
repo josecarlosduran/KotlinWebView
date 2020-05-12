3e841e9c4fbf1d69e1697f426ed3943eea4ead7b
Jose Carlos Duran <josecarlosduran@gmail.com>
2020-05-12 15:27:46 +0200

    Initial Commit

---
+++ app/src/main/java/com/josecarlosduran/mywebview/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.josecarlosduran.mywebview
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.*
+import android.widget.SearchView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    private val BASE_URL = "https://google.com"
+    private val SEARCH_PATH = "/search?q="
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000) // its only for educational purpose
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Refresh
+
+        swipeRefresh.setOnRefreshListener {
+            webView.reload()
+        }
+
+        // search
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let{
+                    if(URLUtil.isValidUrl(it)){
+                        webView.loadUrl(it)
+                    }
+                    else{
+                        webView.loadUrl("$BASE_URL$SEARCH_PATH$it")
+                    }
+                }
+                return false
+            }
+        })
+
+        //webView
+        webView.webChromeClient = object : WebChromeClient() {
+
+        }
+
+        webView.webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                searchView.setQuery(url,false)
+                swipeRefresh.isRefreshing = true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                swipeRefresh.isRefreshing = false
+            }
+
+        }
+
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+
+        webView.loadUrl(BASE_URL)
+
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack()
+        }
+        else{
+            super.onBackPressed()
+        }
+
+    }
+}
